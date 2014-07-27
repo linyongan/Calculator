c2acb37c13ee585f83142c2b6aff565dd6ab1add
linyongan <1061707480@qq.com>
2014-07-27 09:21:37 +0800

    基本UI、个人理财和贷款计算功能
    做好了基本的UI界面，加入了个人理财和贷款计算功能，个人理财可以计算现值，终值，年金现值，年金终值；贷款计算可以计算等额本息，等额本金。

---
+++ src/org/crazyit/activity/InvestmentActivity.java
@@ -0,0 +1,53 @@
+package org.crazyit.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
+
+/**
+ * 投资指标页面
+ */
+public class InvestmentActivity extends Activity
+{
+	/** 返回按钮 */
+	private ImageButton backButton;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		/* 设置全屏 */
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.investment);
+		backButton = (ImageButton) findViewById(R.id.investment_back_bn);
+		backButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				goBack();
+
+			}
+		});
+	}
+
+	/** 退出按钮 */
+	public boolean dispatchKeyEvent(KeyEvent event) {
+		if (event.getKeyCode() == KeyEvent.KEYCODE_BACK) {
+			if (event.getAction() == KeyEvent.ACTION_DOWN
+					&& event.getRepeatCount() == 0) {
+				goBack();
+				return true;
+			}
+		}
+		return super.dispatchKeyEvent(event);
+	}
+
+	/** 返回上一个界面 */
+	private void goBack() {
+		Intent intent = new Intent(InvestmentActivity.this, MainActivity.class);
+		startActivity(intent);
+		finish();
+	}
+}
