5a8fd0c0f565c8279b271b843afd5c73f9ae65ae
linyongan <1061707480@qq.com>
2014-07-28 15:14:06 +0800

    增加名词搜索的listView的字母索引

--- gen/com/linyongan/activity/R.java
+++ gen/com/linyongan/activity/R.java
@@ -12,6 +12,7 @@ public final class R {
         public static final int array1=0x7f040000;
         public static final int array2=0x7f040001;
         public static final int array3=0x7f040002;
+        public static final int date=0x7f040003;
     }
     public static final class attr {
     }
@@ -20,122 +21,135 @@ public final class R {
         public static final int btn_common_selector=0x7f020001;
         public static final int common_btn=0x7f020002;
         public static final int common_btn_pressed=0x7f020003;
-        public static final int icon=0x7f020004;
-        public static final int shape=0x7f020005;
-        public static final int titlebar=0x7f020006;
+        public static final int emotionstore_progresscancelbtn=0x7f020004;
+        public static final int icon=0x7f020005;
+        public static final int search_bar_edit_normal=0x7f020006;
+        public static final int search_bar_edit_pressed=0x7f020007;
+        public static final int search_bar_edit_selector=0x7f020008;
+        public static final int search_bar_icon_normal=0x7f020009;
+        public static final int shape=0x7f02000a;
+        public static final int show_head_toast_bg=0x7f02000b;
+        public static final int sidebar_background=0x7f02000c;
+        public static final int titlebar=0x7f02000d;
     }
     public static final class id {
-        public static final int Button1_back_bn=0x7f060007;
-        public static final int Button1_calculate_bt=0x7f060012;
-        public static final int Button1_clean_bn=0x7f060008;
-        public static final int Button1_editText1=0x7f06000c;
-        public static final int Button1_editText2=0x7f06000d;
-        public static final int Button1_editText3=0x7f06000e;
-        public static final int Button1_editText4=0x7f06000f;
-        public static final int Button1_editText5=0x7f060010;
-        public static final int Button1_out_tv=0x7f060011;
-        public static final int Button1_radioButton1=0x7f06000a;
-        public static final int Button1_radioButton2=0x7f06000b;
-        public static final int Button1_radioGroup=0x7f060009;
-        public static final int Button2_back_bn=0x7f060013;
-        public static final int Button2_calculate_bt=0x7f060025;
-        public static final int Button2_clean_bn=0x7f060014;
-        public static final int Button2_editText1=0x7f06001d;
-        public static final int Button2_editText2=0x7f06001e;
-        public static final int Button2_editText3=0x7f060020;
-        public static final int Button2_editText4=0x7f060021;
-        public static final int Button2_editText5=0x7f060023;
-        public static final int Button2_out_tv=0x7f060024;
-        public static final int Button2_radioButton1=0x7f060016;
-        public static final int Button2_radioButton2=0x7f060017;
-        public static final int Button2_radioButton3=0x7f060019;
-        public static final int Button2_radioButton4=0x7f06001a;
-        public static final int Button2_radioButton5=0x7f06001b;
-        public static final int Button2_radioGroup1=0x7f060015;
-        public static final int Button2_radioGroup2=0x7f060018;
-        public static final int Button2_tableRow1=0x7f06001c;
-        public static final int Button2_tableRow2=0x7f06001f;
-        public static final int Button2_tableRow3=0x7f060022;
-        public static final int Button3_back_bn=0x7f060026;
-        public static final int Button3_calculate_bt=0x7f060029;
-        public static final int Button3_clean_bn=0x7f060027;
-        public static final int Button3_out_tv=0x7f060028;
-        public static final int Button4_back_bn=0x7f06002a;
-        public static final int Button4_calculate_bt=0x7f060037;
-        public static final int Button4_clean_bn=0x7f06002b;
-        public static final int Button4_editText1=0x7f060032;
-        public static final int Button4_editText2=0x7f060033;
-        public static final int Button4_editText3=0x7f060034;
-        public static final int Button4_editText4=0x7f060035;
-        public static final int Button4_out_tv=0x7f060036;
-        public static final int Button4_radioButton1=0x7f06002d;
-        public static final int Button4_radioButton2=0x7f06002e;
-        public static final int Button4_radioButton3=0x7f060030;
-        public static final int Button4_radioButton4=0x7f060031;
-        public static final int Button4_radioGroup1=0x7f06002c;
-        public static final int Button4_radioGroup2=0x7f06002f;
-        public static final int Button5_back_bn=0x7f060038;
-        public static final int Button5_calculate_bt=0x7f060040;
-        public static final int Button5_clean_bn=0x7f060039;
-        public static final int Button5_editText1=0x7f06003a;
-        public static final int Button5_editText2=0x7f06003b;
-        public static final int Button5_editText3=0x7f06003c;
-        public static final int Button5_editText4=0x7f06003d;
-        public static final int Button5_editText5=0x7f06003e;
-        public static final int Button5_out_tv=0x7f06003f;
-        public static final int aboutButton=0x7f06006d;
-        public static final int about_back_bn=0x7f060000;
-        public static final int derivativeButton=0x7f06006b;
-        public static final int derivative_Button1=0x7f060002;
-        public static final int derivative_Button2=0x7f060003;
-        public static final int derivative_Button3=0x7f060004;
-        public static final int derivative_Button4=0x7f060005;
-        public static final int derivative_Button5=0x7f060006;
-        public static final int derivative_back_bn=0x7f060001;
-        public static final int derivative_clean_bn=0x7f060057;
-        public static final int financeButton=0x7f060066;
-        public static final int finance_RadioButton1=0x7f060044;
-        public static final int finance_RadioButton2=0x7f060045;
-        public static final int finance_RadioButton3=0x7f060046;
-        public static final int finance_RadioButton4=0x7f060047;
-        public static final int finance_RadioGroup=0x7f060043;
-        public static final int finance_annuity_et=0x7f06004c;
-        public static final int finance_annuity_tv=0x7f06004b;
-        public static final int finance_back_bn=0x7f060041;
-        public static final int finance_calculate_bt=0x7f060055;
-        public static final int finance_clean_bn=0x7f060042;
-        public static final int finance_finalValue_et=0x7f060050;
-        public static final int finance_finalValue_tv=0x7f06004f;
-        public static final int finance_month_rb=0x7f060052;
-        public static final int finance_out_tv=0x7f060054;
-        public static final int finance_presentValue_et=0x7f06004e;
-        public static final int finance_presentValue_tv=0x7f06004d;
-        public static final int finance_rate_et=0x7f06004a;
-        public static final int finance_year_et=0x7f060049;
-        public static final int finance_year_rb=0x7f060053;
-        public static final int finance_year_tv=0x7f060048;
-        public static final int identificationButton=0x7f060069;
-        public static final int identification_back_bn=0x7f060056;
-        public static final int investmentButton=0x7f060068;
-        public static final int investment_back_bn=0x7f06005b;
-        public static final int learnButton=0x7f06006a;
-        public static final int learn_back_bn=0x7f06005d;
-        public static final int loanButton=0x7f060067;
-        public static final int loan_RadioButton1=0x7f060061;
-        public static final int loan_RadioButton2=0x7f060062;
-        public static final int loan_RadioGroup=0x7f060060;
-        public static final int loan_back_bn=0x7f06005e;
-        public static final int loan_calculate_bt=0x7f060059;
-        public static final int loan_clean_bn=0x7f06005f;
-        public static final int loan_month_et=0x7f060064;
-        public static final int loan_out_tv=0x7f060058;
-        public static final int loan_principal_et=0x7f060063;
-        public static final int loan_yearRate_et=0x7f060065;
-        public static final int news_list_title=0x7f06005c;
-        public static final int news_list_title_bar=0x7f06005a;
-        public static final int period=0x7f060051;
-        public static final int searchButton=0x7f06006c;
-        public static final int search_back_bn=0x7f06006e;
+        public static final int Button1_back_bn=0x7f070007;
+        public static final int Button1_calculate_bt=0x7f070012;
+        public static final int Button1_clean_bn=0x7f070008;
+        public static final int Button1_editText1=0x7f07000c;
+        public static final int Button1_editText2=0x7f07000d;
+        public static final int Button1_editText3=0x7f07000e;
+        public static final int Button1_editText4=0x7f07000f;
+        public static final int Button1_editText5=0x7f070010;
+        public static final int Button1_out_tv=0x7f070011;
+        public static final int Button1_radioButton1=0x7f07000a;
+        public static final int Button1_radioButton2=0x7f07000b;
+        public static final int Button1_radioGroup=0x7f070009;
+        public static final int Button2_back_bn=0x7f070013;
+        public static final int Button2_calculate_bt=0x7f070025;
+        public static final int Button2_clean_bn=0x7f070014;
+        public static final int Button2_editText1=0x7f07001d;
+        public static final int Button2_editText2=0x7f07001e;
+        public static final int Button2_editText3=0x7f070020;
+        public static final int Button2_editText4=0x7f070021;
+        public static final int Button2_editText5=0x7f070023;
+        public static final int Button2_out_tv=0x7f070024;
+        public static final int Button2_radioButton1=0x7f070016;
+        public static final int Button2_radioButton2=0x7f070017;
+        public static final int Button2_radioButton3=0x7f070019;
+        public static final int Button2_radioButton4=0x7f07001a;
+        public static final int Button2_radioButton5=0x7f07001b;
+        public static final int Button2_radioGroup1=0x7f070015;
+        public static final int Button2_radioGroup2=0x7f070018;
+        public static final int Button2_tableRow1=0x7f07001c;
+        public static final int Button2_tableRow2=0x7f07001f;
+        public static final int Button2_tableRow3=0x7f070022;
+        public static final int Button3_back_bn=0x7f070026;
+        public static final int Button3_calculate_bt=0x7f070029;
+        public static final int Button3_clean_bn=0x7f070027;
+        public static final int Button3_out_tv=0x7f070028;
+        public static final int Button4_back_bn=0x7f07002a;
+        public static final int Button4_calculate_bt=0x7f070037;
+        public static final int Button4_clean_bn=0x7f07002b;
+        public static final int Button4_editText1=0x7f070032;
+        public static final int Button4_editText2=0x7f070033;
+        public static final int Button4_editText3=0x7f070034;
+        public static final int Button4_editText4=0x7f070035;
+        public static final int Button4_out_tv=0x7f070036;
+        public static final int Button4_radioButton1=0x7f07002d;
+        public static final int Button4_radioButton2=0x7f07002e;
+        public static final int Button4_radioButton3=0x7f070030;
+        public static final int Button4_radioButton4=0x7f070031;
+        public static final int Button4_radioGroup1=0x7f07002c;
+        public static final int Button4_radioGroup2=0x7f07002f;
+        public static final int Button5_back_bn=0x7f070038;
+        public static final int Button5_calculate_bt=0x7f070040;
+        public static final int Button5_clean_bn=0x7f070039;
+        public static final int Button5_editText1=0x7f07003a;
+        public static final int Button5_editText2=0x7f07003b;
+        public static final int Button5_editText3=0x7f07003c;
+        public static final int Button5_editText4=0x7f07003d;
+        public static final int Button5_editText5=0x7f07003e;
+        public static final int Button5_out_tv=0x7f07003f;
+        public static final int aboutButton=0x7f07006f;
+        public static final int about_back_bn=0x7f070000;
+        public static final int catalog=0x7f07005d;
+        public static final int country_lvcountry=0x7f070072;
+        public static final int derivativeButton=0x7f07006d;
+        public static final int derivative_Button1=0x7f070002;
+        public static final int derivative_Button2=0x7f070003;
+        public static final int derivative_Button3=0x7f070004;
+        public static final int derivative_Button4=0x7f070005;
+        public static final int derivative_Button5=0x7f070006;
+        public static final int derivative_back_bn=0x7f070001;
+        public static final int derivative_clean_bn=0x7f070057;
+        public static final int dialog=0x7f070073;
+        public static final int filter_edit=0x7f070071;
+        public static final int financeButton=0x7f070068;
+        public static final int finance_RadioButton1=0x7f070044;
+        public static final int finance_RadioButton2=0x7f070045;
+        public static final int finance_RadioButton3=0x7f070046;
+        public static final int finance_RadioButton4=0x7f070047;
+        public static final int finance_RadioGroup=0x7f070043;
+        public static final int finance_annuity_et=0x7f07004c;
+        public static final int finance_annuity_tv=0x7f07004b;
+        public static final int finance_back_bn=0x7f070041;
+        public static final int finance_calculate_bt=0x7f070055;
+        public static final int finance_clean_bn=0x7f070042;
+        public static final int finance_finalValue_et=0x7f070050;
+        public static final int finance_finalValue_tv=0x7f07004f;
+        public static final int finance_month_rb=0x7f070052;
+        public static final int finance_out_tv=0x7f070054;
+        public static final int finance_presentValue_et=0x7f07004e;
+        public static final int finance_presentValue_tv=0x7f07004d;
+        public static final int finance_rate_et=0x7f07004a;
+        public static final int finance_year_et=0x7f070049;
+        public static final int finance_year_rb=0x7f070053;
+        public static final int finance_year_tv=0x7f070048;
+        public static final int identificationButton=0x7f07006b;
+        public static final int identification_back_bn=0x7f070056;
+        public static final int investmentButton=0x7f07006a;
+        public static final int investment_back_bn=0x7f07005b;
+        public static final int learnButton=0x7f07006c;
+        public static final int learn_back_bn=0x7f07005f;
+        public static final int loanButton=0x7f070069;
+        public static final int loan_RadioButton1=0x7f070063;
+        public static final int loan_RadioButton2=0x7f070064;
+        public static final int loan_RadioGroup=0x7f070062;
+        public static final int loan_back_bn=0x7f070060;
+        public static final int loan_calculate_bt=0x7f070059;
+        public static final int loan_clean_bn=0x7f070061;
+        public static final int loan_month_et=0x7f070066;
+        public static final int loan_out_tv=0x7f070058;
+        public static final int loan_principal_et=0x7f070065;
+        public static final int loan_yearRate_et=0x7f070067;
+        public static final int news_list_title=0x7f07005c;
+        public static final int news_list_title_bar=0x7f07005a;
+        public static final int period=0x7f070051;
+        public static final int searchButton=0x7f07006e;
+        public static final int search_back_bn=0x7f070070;
+        public static final int sidrbar=0x7f070074;
+        public static final int title=0x7f07005e;
     }
     public static final class layout {
         public static final int about=0x7f030000;
@@ -148,13 +162,31 @@ public final class R {
         public static final int finance=0x7f030007;
         public static final int identification=0x7f030008;
         public static final int investment=0x7f030009;
-        public static final int learn=0x7f03000a;
-        public static final int loan=0x7f03000b;
-        public static final int main=0x7f03000c;
-        public static final int search=0x7f03000d;
+        public static final int item=0x7f03000a;
+        public static final int learn=0x7f03000b;
+        public static final int loan=0x7f03000c;
+        public static final int main=0x7f03000d;
+        public static final int search=0x7f03000e;
     }
     public static final class string {
         public static final int app_name=0x7f050001;
         public static final int tip=0x7f050000;
+    }
+    public static final class style {
+        /** 
+        Base application theme, dependent on API level. This theme is replaced
+        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+    
+
+            Theme customizations available in newer API levels can go in
+            res/values-vXX/styles.xml, while customizations related to
+            backward-compatibility can go here.
+        
+         */
+        public static final int AppBaseTheme=0x7f060000;
+        /**  Application theme. 
+ All customizations that are NOT specific to a particular API-level can go here. 
+         */
+        public static final int AppTheme=0x7f060001;
     }
 }
